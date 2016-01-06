ce853dd9d0819bbc93ff8ada9c2e47eb3a7cadbc
Kelby Hunt <huntk20@gmail.com>
2016-01-06 12:42:54 -0800

    Working version of extension for Font Awesome and Bootstrap glyph icons

---
+++ ResharperGlyphfriend/GlyphIconProvider.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.ReSharper.Psi;
+using JetBrains.UI.Icons;
+using JetBrains.UI.Icons.ImageSourceIcons;
+
+
+namespace ResharperGlyphfriend
+{
+    [DeclaredElementIconProvider]
+    public class GlyphIconProvider : IDeclaredElementIconProvider
+    {
+        public IconId GetImageId(IDeclaredElement declaredElement, PsiLanguageType languageType, out bool canApplyExtensions)
+        {
+            if (declaredElement.GetElementType().PresentableName == @"css class")
+            {
+                canApplyExtensions = false;
+
+                ImageSourceIconId value;
+                if (GlyphfriendStorage.Glyphs.TryGetValue(declaredElement.ShortName, out value))
+                {
+                    return GlyphfriendStorage.Glyphs[declaredElement.ShortName];
+                }
+            }
+
+            canApplyExtensions = true;
+
+            return null;
+        }
+    }
+}
---
+++ ResharperGlyphfriend/GlyphfriendStorage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using JetBrains.ProjectModel;
+using JetBrains.UI.Icons.ImageSourceIcons;
+
+namespace ResharperGlyphfriend
+{
+    [SolutionComponent]
+    public class GlyphfriendStorage
+    {
+        private static Dictionary<string, ImageSourceIconId> _glyphs;
+
+        internal static Dictionary<string, ImageSourceIconId> Glyphs
+        {
+            get { return _glyphs; }
+            set { _glyphs = value; }
+        }
+
+        public GlyphfriendStorage()
+        {
+            LoadGlyphs();
+        }
+
+        private void LoadGlyphs()
+        {
+            _glyphs = _glyphs ?? new Dictionary<string, ImageSourceIconId>();
+
+            foreach (var manifestResourceName in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+            {
+                string exactResourceName = manifestResourceName.Split('.')[3];
+
+                var glyphStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(manifestResourceName);
+
+                if (glyphStream != null)
+                {
+                    var glyphsBitmap = BitmapFrame.Create(glyphStream);
+
+                    _glyphs.Add(exactResourceName, new ImageSourceIconId(glyphsBitmap));
+                }
+            }
+        }
+    }
+}
